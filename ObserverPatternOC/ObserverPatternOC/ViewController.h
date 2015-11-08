7cf2b3a628e6f6372893f4f409ea65b8a093d37b
张青明 <zhangqingming@codinghacker.com>
2015-11-08 23:23:23 +0800

    创建工程

---
+++ ObserverPatternOC/ObserverPatternOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObserverPatternOC
+//
+//  Created by 张青明 on 15/11/8.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObserverPatternOC/ObserverPatternOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObserverPatternOC
+//
+//  Created by 张青明 on 15/11/8.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
